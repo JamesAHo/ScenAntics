3a97aa6f75a3a8dd1eaec9b31d35b02ca6c393b2
Sabrina LeMay <b33na1997@gmail.com>
2022-10-03 14:23:19 -0700

    added header component to login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,9 +3,17 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN_QUERY } from '../utils/queries';
 import {Auth} from "../utils/Authentication"
+import Header from '../components/Header';
 
 export default function LoginPage(){
     return(
-        <></>
+        <>
+            <Header
+                heading = "Please login"
+                paragraph = "If you don't have an account please signup."
+                linkName = "Signup"
+                linkUrl = "/signup"
+                />
+        </>
     )
 }
