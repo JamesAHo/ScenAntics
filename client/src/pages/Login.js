d0e669714b6a00544f995ea2a097f838571d055c
Sabrina LeMay <b33na1997@gmail.com>
2022-10-03 13:31:05 -0700

    added boiler plate code to login.js in pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,3 +3,9 @@ import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { LOGIN_QUERY } from '../utils/queries';
 import {Auth} from "../utils/Authentication"
+
+export default function LoginPage(){
+    return(
+        <></>
+    )
+}
