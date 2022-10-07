2da5853613aa556bbc8e81145bd25cd3fe8c21e9
Sabrina LeMay <b33na1997@gmail.com>
2022-10-06 23:40:26 -0700

    deleted commented code in app,js,  added daisyui to tailwind and ran npm i to install. Added code to Gamepage to have game content show and timer

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -1,19 +1,18 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function playGame({
-    type = 'submit',
-    className = '',
-    processing,
-  }) {
-    return (
-      <Link to="/genre">
+export default function PlayGame() {
+  const [show, setShow] = useState(true)
+    const hideButton = () => {
+      setShow(!show)
+    }
+
+  return (
+    <Link to="/genre">
       {show && <button
         type="submit"
         className={
-          `inline-flex items-center h-40 w-full px-20 py-6 bg-green-600 border border-transparent rounded-md font-semibold text-2xl text-white uppercase tracking-widest active:bg-green-900 transition ease-in-out duration-150 ${
-            processing && 'opacity-25'
-          } ` + className
+          `inline-flex items-center h-40 w-full px-20 py-6 bg-green-600 border border-transparent rounded-md font-semibold text-2xl text-white uppercase tracking-widest active:bg-green-900 transition ease-in-out duration-150` 
         }
         onClick={() => {
           hideButton()
@@ -22,7 +21,7 @@ export default function playGame({
         Start
       </button>
       }
-      </Link>
-    );
-  }
+    </Link>
+  );
+}
 
