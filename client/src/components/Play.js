402ed91ac444af223409db603173b54637e63db1
James Anh <james.anh184@gmail.com>
2022-10-08 00:22:21 -0700

    added daisy UI

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -1,6 +1,6 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
-
+import Auth from "../utils/Authentication"
 export default function PlayGame() {
   const [show, setShow] = useState(true)
   const hideButton = () => {
@@ -13,7 +13,7 @@ export default function PlayGame() {
       {show && <button
         type="submit"
         className=
-        "flex justify-center text-center h-30 w-6 px-20 py-5  bg-green-600 border border-transparent rounded-md font-semibold text-2xl text-white uppercase tracking-widest active:bg-green-900 transition ease-in-out duration-150"
+        "flex justify-center text-center  h-30 w-6 px-20 py-5  bg-green-600 border border-transparent rounded-md font-semibold text-2xl text-white uppercase tracking-widest active:bg-green-900 transition ease-in-out duration-150 hover:bg-sky-700"
 
         onClick={() => {
           hideButton()
